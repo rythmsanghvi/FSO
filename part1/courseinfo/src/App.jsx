2f15567a8b1fa3b9a2abe0b63529322bb2e25c78
Rythm Sanghvi <sanghvi.rythm7402@gmail.com>
2026-06-28 23:47:10 -0700

    Completed courseinfo excercise

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,60 @@
+const Header = (props) => {
+  return (
+    <>
+    <h1>{props.course.name}</h1>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+    <Part part={props.parts[0]} />
+    <Part part={props.parts[1]} />
+    <Part part={props.parts[2]} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of excercises {props.parts[0].excercises + props.parts[1].excercises + props.parts[2].excercises}</p>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+        {props.part.name} {props.part.excercises}
+    </p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        excercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        excercises: 7
+      },
+      {
+        name: 'State of component',
+        excercises: 14
+      }]
+    }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
